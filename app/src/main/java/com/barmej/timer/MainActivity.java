18c6058ab3cb9d717b58be12fae043dab1589868
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2020-03-03 10:17:18 +0100

    new modification

--- app/src/main/java/com/barmej/timer/MainActivity.java
+++ app/src/main/java/com/barmej/timer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.barmej.timer;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -10,52 +11,59 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnstart, btnstop;
-    ImageView icanchor;
-    Animation roundingalone;
-    Chronometer timerHere;
+     TextView tvSplash, tvSubSplash;
+     Button btnget;
+     Animation atg, btgone, btgtwo;
+     ImageView ivSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        btnstart = findViewById(R.id.btnstart);
-        icanchor = findViewById(R.id.icanchor);
-        btnstop = findViewById(R.id.btnstop);
-        timerHere = findViewById(R.id.timerHere);
-
-
-
-        // create optional animation
-        btnstop.setAlpha(0);
-
-        //load animations
-        roundingalone = AnimationUtils.loadAnimation(this, R.anim.roundingalone);
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnget = findViewById(R.id.btnget);
+        ivSplash = findViewById(R.id.ivSplash);
 
 
         //import font
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
         Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.ttf");
+
+        // custumer font
+        tvSplash.setTypeface(MRegular);
+        tvSubSplash.setTypeface(MLight);
+        btnget.setTypeface(MMedium);
+
+        //load animations
+        atg = AnimationUtils.loadAnimation(this, R.anim.atg);
+        btgone = AnimationUtils.loadAnimation(this, R.anim.btgone);
+        btgtwo = AnimationUtils.loadAnimation(this, R.anim.btgtwo);
 
 
-        //customize font
-        btnstart.setTypeface(MMedium);
-        btnstop.setTypeface(MMedium);
-        btnstart.setOnClickListener(new View.OnClickListener() {
+        // passing  animation
+        ivSplash.startAnimation(atg);
+        tvSplash.startAnimation(btgone);
+        tvSubSplash.startAnimation(btgone);
+        btnget.startAnimation(btgtwo);
+
+
+        btnget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //passing animation
-                icanchor.startAnimation(roundingalone);
-                btnstop.animate().alpha(1).translationY(-80).setDuration(300).start();
-                btnstop.animate().alpha(1).setDuration(300).start();
+                Intent a = new Intent(MainActivity.this, StopWatchAct.class);
+                a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(a);
 
-                //start time
-                timerHere.setBase(SystemClock.elapsedRealtime());
-                timerHere.start();
             }
         });
+
+
+
     }
 }
